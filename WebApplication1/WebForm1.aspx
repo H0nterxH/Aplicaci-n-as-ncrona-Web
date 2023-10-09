06b219e0ade03bff1d21fb589c304b936e49a917
H0nterxH <133892990+H0nterxH@users.noreply.github.com>
2023-10-09 10:08:42 -0600

    Add files via upload

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Consulta de Datos</title>
+    
+    <style>
+        body {
+            background: linear-gradient(to bottom, red, white);
+            text-align: center;
+        }
+
+        .custom-button {
+            width: 150px; 
+            padding: 10px; 
+            background-color: black;
+            color: white;
+            text-align: left; 
+            display: block; 
+            margin: 5px 0; 
+        }
+    </style>
+</head>
+<body>
+    <h3>Practica</h3>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server" />
+            <asp:Button ID="Button1" runat="server" Text="Consulta 1" OnClick="Button1_Click" CssClass="custom-button" />
+            <asp:Button ID="Button2" runat="server" Text="Consulta 2" OnClick="Button2_Click" CssClass="custom-button" />
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                    <asp:GridView ID="GridView1" runat="server" BackColor="white"></asp:GridView>
+                </ContentTemplate>
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="Button1" EventName="Click" />
+                    <asp:AsyncPostBackTrigger ControlID="Button2" EventName="Click" />
+                </Triggers>
+            </asp:UpdatePanel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Inicialmente, el GridView está vacío.
+                GridView1.DataSource = null;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-PTBLD39\\SQLEXPRESS;Initial Catalog=AdventureWorks2012;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"
+                    SELECT
+                        po.PurchaseOrderID,
+                        po.VendorID,
+                        pod.ProductID,
+                        pod.UnitPrice,
+                        pod.StockedQty,     
+                    (SELECT Name FROM Purchasing.ShipMethod WHERE ShipMethodID = po.ShipMethodID) AS ShipMethodName,
+                    (SELECT ShipBase FROM Purchasing.ShipMethod WHERE ShipMethodID = po.ShipMethodID) AS ShipBase
+                    FROM Purchasing.PurchaseOrderDetail AS pod
+                    INNER JOIN Purchasing.PurchaseOrderHeader AS po ON pod.PurchaseOrderID = po.PurchaseOrderID
+                    WHERE po.VendorID = 1662;";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-PTBLD39\\SQLEXPRESS;Initial Catalog=AdventureWorks2012;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"
+                    SELECT 
+                    po.PurchaseOrderID,
+                    po.VendorID,
+                    po.SubTotal,
+                    pod.UnitPrice,
+                    pod.OrderQty,
+                    p.ProductID,
+                    p.Name AS ProductName,
+                    p.Color,
+                    v.Name AS VendorName,
+                    v.AccountNumber
+                FROM Purchasing.PurchaseOrderHeader AS po
+                INNER JOIN Purchasing.PurchaseOrderDetail AS pod ON po.PurchaseOrderID = pod.PurchaseOrderID
+                INNER JOIN Production.Product AS p ON pod.ProductID = p.ProductID
+                INNER JOIN Purchasing.Vendor AS v ON po.VendorID = po.VendorID
+                WHERE po.VendorID BETWEEN 1568 AND 1572;";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
